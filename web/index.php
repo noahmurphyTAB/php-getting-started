6276019f87a6f6f316ebd581ac6780a98d70992e
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 16:56:22 +0100

    no message

--- web/index.php
+++ web/index.php
@@ -16,14 +16,12 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 $app->get('/', function() use($app) {
   ######## SLACK ###########
     
-  $command = $_POST['command'];           // Slash command - should be /request
-  $request = $_POST['text'];              // Text after Slash command
-  $user_id = $_POST['user_id'];           // Slack user ID
-  $slack_username = $_POST['user_name'];  // Slack username
+          #$command = $_POST['command'];           // Slash command - should be /request
+          #$request = $_POST['text'];              // Text after Slash command
+          #$user_id = $_POST['user_id'];           // Slack user ID
+          #$slack_username = $_POST['user_name'];  // Slack username
 
-  return "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
+  return "Hi there";
 });
 
 $app->run();
-
-?>
