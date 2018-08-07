628c8275b47403cacde0d396dfcc070741b44088
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 15:01:08 +0100

    no message

--- slack-fs.php
+++ slack-fs.php
@@ -3,7 +3,6 @@
     
 $command = $_POST['command'];           // Slash command - should be /request
 $request = $_POST['text'];              // Text after Slash command
-$token = $_POST['token'];               // Slash command verification token
 $user_id = $_POST['user_id'];           // Slack user ID
 $slack_username = $_POST['user_name'];  // Slack username
 
