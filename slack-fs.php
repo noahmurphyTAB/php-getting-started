30e1de9db3095eeeeab93e676a336d591430f530
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 15:05:30 +0100

    no message

--- slack-fs.php
+++ slack-fs.php
@@ -6,5 +6,5 @@ $request = $_POST['text'];              // Text after Slash command
 $user_id = $_POST['user_id'];           // Slack user ID
 $slack_username = $_POST['user_name'];  // Slack username
 
-    #echo "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
+    echo "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
 ?>
