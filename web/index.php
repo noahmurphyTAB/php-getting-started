975a780fee74a9aaaea8db2c37134405e2b1157b
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 16:47:06 +0100

    no message

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  ######## SLACK ###########
+    
+  $command = $_POST['command'];           // Slash command - should be /request
+  $request = $_POST['text'];              // Text after Slash command
+  $user_id = $_POST['user_id'];           // Slack user ID
+  $slack_username = $_POST['user_name'];  // Slack username
+
+  return "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
+});
+
+$app->run();
