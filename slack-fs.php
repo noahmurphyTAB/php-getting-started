9f5f39a3edfc1da64bc88808d52316cc0ec7a063
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 14:54:37 +0100

    no message

--- slack-fs.php
+++ slack-fs.php
@@ -7,5 +7,5 @@ $token = $_POST['token'];               // Slash command verification token
 $user_id = $_POST['user_id'];           // Slack user ID
 $slack_username = $_POST['user_name'];  // Slack username
 
-    echo "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
+    #echo "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
 ?>
