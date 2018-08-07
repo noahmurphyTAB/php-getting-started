d87ece2f7b405fbb50d8a122a3eeddedb31f364b
Noah Murphy <noah.murphy@theappbusiness.com>
2018-08-07 14:07:49 +0100

    no message

--- slack-fs.php
+++ slack-fs.php
@@ -1,14 +1,11 @@
 <?php
 ######## SLACK ###########
-
+    
 $command = $_POST['command'];           // Slash command - should be /request
 $request = $_POST['text'];              // Text after Slash command
 $token = $_POST['token'];               // Slash command verification token
 $user_id = $_POST['user_id'];           // Slack user ID
 $slack_username = $_POST['user_name'];  // Slack username
-$user_email = $response_array['user']['profile']['email']; #get email address
-$user_first_name = $response_array['user']['profile']['first_name']; #get first name
 
-
-    echo "Hi ".$user_first_name.". You must be happy to see that you typed: *".$request."*.;
+    echo "Hi ".$slack_username.". You must be happy to see that you typed: *".$request."*.;
 ?>
